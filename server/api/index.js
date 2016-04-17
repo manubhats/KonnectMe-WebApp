eab0f2f7f9f746e1b1f78c024855534c6a1e2aed
Hussain <hussain.mucklai@gmail.com>
2016-04-16 22:11:22 -0500

    Hopeful bug fix

--- server/api/index.js
+++ server/api/index.js
@@ -20,9 +20,9 @@ export default function() {
         req.addListener('end', function(){
             console.log("Creating Tropo");
             let tropo = new TropoWebAPI();
-            console.log(json);
+
             // Create a new instance of the Session object and give it the JSON delivered from Tropo.
-            let session = Session(json);
+            let session = JSON.parse(json).parameters;
             console.log(session);
 
             tropo.call(`${session.recipient_phone_number}`);
