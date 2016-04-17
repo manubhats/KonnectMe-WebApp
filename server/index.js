7a5cc608ceb2bdde87bd83a057aa0f79dd37820a
Hussain <hussain.mucklai@gmail.com>
2016-04-16 19:30:17 -0500

    Removed unused libraries...again.

--- server/index.js
+++ server/index.js
@@ -1,17 +1,9 @@
 import http from 'http';
 import express from 'express';
-import morgan from 'morgan';
 import api from './api';
 
 var app = express();
 app.server = http.createServer(app);
-
-// configure app
-app.use(morgan('dev')); // log requests to the console
-
-// configure body parser
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
 
 app.use(express.static('./client'));
 
