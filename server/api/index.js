ddc8b6eb07b798de5cc8ba794e924a3761fc1197
Revanth Pobala <revanth.linux@gmail.com>
2016-04-17 00:01:30 -0500

    Final Debugg

--- server/api/index.js
+++ server/api/index.js
@@ -23,7 +23,7 @@ export default function() {
 
             // Create a new instance of the Session object and give it the JSON delivered from Tropo.
             let session = (JSON.parse(json)).session.parameters;
-			console.log(session);
+			console.log(json);
             tropo.call(`${session.recipient_phone_number}`);
             
             tropo.say(`Hey ${session.recipient_name}. You have been invited by ${session.initiator_name} to ${session.message}.`);
